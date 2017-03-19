cec8e4f4786704383b34a025f8283d626bf97ee5
Euis <trie.uchul96@gmail.com>
2017-03-19 20:17:12 +0700

    Add files via upload

---
+++ InputOutput1.java
@@ -0,0 +1,7 @@
+public class InputOutput1 {
+public static void main (String [] args){
+System.out.print ("Belajar Java Kuys");
+System.out.println ("Belajar Java Kuys 1");
+System.out.print ("Belajar Java Kuys 2");
+}
+}
---
+++ InputOutput2.java
@@ -0,0 +1,5 @@
+public class InputOutput2 {
+public static void main (String [] args){
+System.out.print ("Belajar \nJava\n Kuys");
+}
+}
